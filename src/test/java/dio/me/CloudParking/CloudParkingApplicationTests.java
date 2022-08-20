7cc4afd1557dcf0db0df3d020de50605cbb95d69
Osny Neto <osnynt@gmail.com>
2022-08-20 02:00:39 -0300

    Definindo versão do java

---
+++ src/test/java/dio/me/CloudParking/CloudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.me.CloudParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
