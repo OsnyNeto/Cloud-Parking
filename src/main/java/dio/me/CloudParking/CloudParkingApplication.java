fbb26ddaa9460aa029bb8944ff68df2176b4a4e2
Osny Neto <osnynt@gmail.com>
2022-08-21 03:09:48 -0300

    Adição de classes e métodos

--- src/main/java/dio/me/CloudParking/CloudParkingApplication.java
+++ src/main/java/dio/me/CloudParking/CloudParkingApplication.java
@@ -2,8 +2,10 @@ package dio.me.CloudParking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+
 public class CloudParkingApplication {
 
 	public static void main(String[] args) {
