be57dd9eea24fbb8f43dc4bff4ebdff7728e3c49
Osny <66511808+OsnyNeto@users.noreply.github.com>
2022-11-05 04:21:21 -0300

    Atualizando arquivos do projeto

---
+++ src/main/java/SpringBoot/CloudParking/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package SpringBoot.CloudParking.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+@RequestMapping("/")
+@ApiIgnore
+public class HelloController {
+    @GetMapping
+    public String hello(){
+        return "Hello Osny Neto";
+    }
+}
